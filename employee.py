e02216626ec6d7fd7e73ee373c92c56907f39ca6
Vaishali <vaishali81patel@gmail.com>
2018-04-02 02:50:06 +1200

    Initialize

---
+++ employee.py
@@ -0,0 +1,15 @@
+# Create an employee class
+
+
+class Employee:
+
+    # Initialize the attribute
+    def __init__(self, empid, gender, age, sales, bmi, salary, birthday):
+        self.empid = empid
+        self.gender = gender
+        self.age = age
+        self.sales = sales
+        self.bmi = bmi
+        self.salary = salary
+        self.birthday = birthday
+
