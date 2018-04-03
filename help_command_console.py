f7056e0f556e1085dd7df4e84c049206e20e2f37
Vaishali <vaishali81patel@gmail.com>
2018-04-03 21:07:31 +1200

    Completed

--- help_command_console.py
+++ help_command_console.py
@@ -2,6 +2,7 @@ from cmd import Cmd
 from i_database import IDatabase
 from employee import Employee
 from csv import Error as CSVError
+from data_validator import DataValidator
 # from view_console import ViewConsole as View
 # from employee_data import Employee_data
 import string
@@ -24,95 +25,263 @@ class CLI(Cmd):
         # Instance of EmployeeData
         # self._std = EmployeeData()
 
-    def do_help(self, args):
-        """Get help on commands
-           'help' or '?' with no arguments prints a \
-           list of commands for which help is available
-           'help <command>' or '? <command>' gives help on <command>
+
+def do_select(self, line):
+    """
+    Select data from source
+    :param self:
+    :param line:
+    :return:
+    """
+    # Available data source
+    options = "-csv", "-db"
+    args = list(arg.lower() for arg in str(line).split())
+
+    try:
+        # Check if the input data source is available in this program or not
+        if args[0] not in options:
+            raise ValueError("The data source is not available")
+        else:
+            # Code for initialise CSV data source
+            if args[0] == "-csv":
+                try:
+                    if len(args) == 1:
+                        self._std.select_source(args[0][1:], "employeeInfo.csv")
+                        View.warning("No CSV file path specified. A default file \"employee.csv\ will be used.")
+                    elif len(args) == 2:
+                        self._std.select_source(args[0][1:], args[1])
+                    elif len(args) == 3:
+                        if args[1] == "-a":
+                            self._std.select_source(args[0][1:], args[2], True)
+                except (CSVError, OSError) as e:
+                    View.error(e)
+                except Exception as e:
+                    View.error(e)
+                else:
+                    View.Success("Daa source CSV is selected.")
+
+            # Code for initialize database source
+            elif args[0] == "-db":
+                try:
+                    self._std.select_source(args[0][1:])
+                except (ConnectionAbortedError, TypeError) as e:
+                    View.error(e)
+                except Exception as e:
+                    View.error(e)
+                else:
+                    View.Success("Data source Database is selected.")
+
+            # Code for initialixe XXXX data source
+            else:
+                pass
+    #Catch and display error message
+    except ValueError as e:
+        View.error(str(e) + "\n")
+        View.help_select()
+    except Exception as e:
+        View.error(e)
+
+    def do_add(self, line):
         """
-        # The only reason to define this method is \
-        # for the help text in the doc string
-        cmd.Cmd.do_help(self, args)
+        Add anew entry of data
+        :param self:
+        :param line:
+        :return:
+        """
+        # Split the input argument to obtain the data
+        raw_data = list(arg.lower() for arg in str(line).split())
 
-    def do_quit(self, arg):
-        """ Exits from the Console!"""
-        sys.exit(1)
+        try:
+            # Check if input data has 7 data fields
+            if not len(raw_data) == len(Employee):
+                raise AttributeError("Please input correct data.")
+            else:
+                # Check and wash data by check_all() of DataValidator
+                result = self._valid.check_all(raw_data)
+                # Check if there is any None which stands for invalid input
+                if None in result:
+                    key = 0
+                    # build a list of name list
+                    items = list(map(lambda i: i.name, Employee))
+                    e_str = ""
+                    while key < len(result):
+                        if result[key] is None:
+                            # Left alignment
+                            e_str += "{:<10}".format(items[key])
+                        key += 1
+                    raise ValueError("The following field(s) is invalid:\n%s" % e_str)
+                else:
+                    self._std.add_data(result)
+        except (AttributeError, ValueError) as e:
+            View.error(str(e) + "\n")
+            View.help_add()
+        except CSVError as e:
+            View.error(e)
+        except Exception as e:
+            View.error(e)
+        else:
+            View.success("Add data")
 
-    def help_quit(self):
-        """ Get the help on Quit"""
-        print('THANKS FOR USING CONSOLE! BYE')
-        sys.exit(1)
 
-    # shortcuts
-    do_q = do_quit
+    def do_save(self, arg):
+        """
+        Save data to specified sata source
+        :param self:
+        :param arg:
+        :return:
+        """
+        # If data source selected, prompt user to do so.
+        try:
+            self._std.save_data()
+        except ValueError as e:
+            View.Info(e)
+        except (OSError, AttributeError) as e:
+            View.error(e)
+        except Exception as e:
+            View.error(e)
+        else:
+            View.success("Data is saved.")
 
-    # Command definitions to \
-    # support Cmd object functionality \
-    def do_EOF(self, args):
-        """Exit on system end of file character"""
-        return self.do_exit(self, args)
 
-    def do_shell(self, args):
-        """Pass command to a system shell when line begins with '!'"""
-        os.system(args)
+    def do_show(self, line):
+        """
+        Get all instructions
+        :param self:
+        :param line:
+        :return:
+        """
+        args = list(arg.lower() for arg in str(line).split())
+        # Those commands are required single arguments
+        # single_commands = ["-a"]
+        # Those commands are required two arguments
 
-    def do_add(self, s):
-        print('This command adds the employee data to the system.')
-        print('USAGE:')
-        print('add< EMPID >< GENDER >< AGE >')
-        print ('< SALES >< BMI >< SALARY >< BIRTHDAY >')
-        print("""***DATA FORMAT:
-                    EMPID..........[A-Z][0-9]{3} e.x.: M123
-                    GENDER.........(M|F)  e.x.: F      \
-                    AGE............[0-9]{2}  e.x.: 28  \
-                    SALES..........[0-9]{3} e.x.: 100  \
-                    BMI............(Normal|Overweight|Obesity|Underweight) \
-                                e.x.: Normal
-                    SALARY.........[0-9]{2-3} e.x.: 200
-                    BIRTHDAY.......[1-31]-[1-12]-[0-9]{4} \
-                                e.x.: 1990-10-21' """)
+        plot_commands = ["-p", "-b", "-c"]
 
-    def do_save(self, s):
-        print('save command saves the current employee data to the system.')
-        print('USAGE:')
-        print('save < FILENAME >')
+        # Show data_table
+        if args[0] == "-t":
+            if len(self._std.data) == 0 and len(self._std.new_data) == 0:
+               View.info("No data tp display.")
+            if not len(self._std.data) == 0:
+                View.display("ORIGINAL DATA:")
+                View.display_data(self._std.data, ind=True)
+            if not len(self._std.new_data) == 0:
+                View.display("\nNew DATA:")
+                View.display_data(self._std.new_data, ind=True)
+                View.display("\n(Input command \"save\" to save the new data")
 
-    def do_show(self, s):
-        print('show command displays the current')
-        print('working employee information in a specified format.')
-        print('USAGE:')
-        print('show < -OPTION > < OBJECT >')
+        elif args[0] in plot_commands:
+            try:
+                if len(args) == 1:
+                    raise IndexError("Incomplete command line.")
 
-    def help_add(self):
-        print("""*** OPTIONS
-            -l : This option loads the employee information from a file. \
-                 The file is given to the command as a string.
-            -m : This option does manual data entry. The user will be \
-                 prompted for step by step employee information \
-                 to be entered into the system.
-            -d : This option loads the employee information \
-                 into the system from a database.
-            ***""")
+                if args[0] == "-p"
+                    self.show_pie(args[1])
+                if args[0] == "-b":
+                    self.show_bar(args[1])
+                if args[0] == "-c"
+                    self.show_scatter(args[1])
 
-    def help_save(self):
-        print("""*** OPTIONS
-                -s : This option is a standard save. The information is \
-                     saved to a file in the existing program files.
-                -d : This option saves the current employee \
-                     information to the database.
-                -f : This option saves a file to the specific file location.
-            ***""")
+            except IndexError as e:
+                View.error(str(e) + "\n")
+                View.help_show()
+        else:
+            View.info("Invalid command line.\n")
+            View.help_show()
 
-    def help_show(self):
-        print("""*** OPTIONS
-            -a : This option shows a bar graph of the total sales made \
-                 by males verse the total sales made by female.
-            -b : This option shows a pie chart of the percentage \
-                 of female workers verse male workers
-            -c : This option shows a scatter plot graph of \
-                 peoples age verse their salary.
-            -d : This option shows a pie chart of the BMI of a set of people.
-            ***""")
-cli = CLI()
-cli.cmdloop()
+
+    def show_pie(self, line):
+        """
+        Show Pie chart
+        :param self:
+        :param line:
+        :return:
+        """
+        try:
+            if len(self._std.get_gender()) == 0 or len(self._std.get_bmi()) == 0:
+                raise ValueError("No data to display.")
+            # Draw gender
+            if line.upper() == Employee.GENDER.name:
+                View.plot_pie(self._std.get_gender(), "Gender Distribution")
+            # Draw BMI
+            if line.upper() == Employee.BMI.name:
+                View.plot_pie(self._std.get_bmi(), "Body Mass Index (BMI)")
+        except ValueError as e:
+            View.info(e)
+        except Exception as e:
+            View.error(e)
+
+
+    def show_bar(self, line):
+        """
+        Draw Bar Chart
+        :param self:
+        :param line:
+        :return:
+        """
+        try:
+            if len(self._std.get_gender()) == 0 or len(self._std.get_gender()) == 0:
+                raise ValueError("No data to display.")
+            # Draw gender
+            if line.upper() == Employee.GENDER.name:
+                View.plot_bar(self._std.get_bmi(), "Body Mass Index (BMI)")
+        except ValueError as e:
+            View.info(e)
+        except Exception as e:
+            View.error(e)
+
+
+    def show_Scatter(Self, line):
+        """
+        Draw Scatter Chart
+        :param Self:
+        :param line:
+        :return:
+        """
+        try:
+            if len (self._std.get_gender ()) == 0 or len (self._std.get_gender ()) == 0:
+                raise ValueError ("No data to display.")
+            # Draw gender
+            if line.upper () == Data.GENDER.name:
+                View.plot_bar (self._std.get_gender (), "Gender Distribution")
+            # Draw BMI
+            if line.upper () == Data.SALARY.name:
+                View.plot_bar (self._std.get_salary (), "Body Mass Index (SALARY)")
+        except ValueError as e:
+            View.info (e)
+        except Exception as e:
+            View.error (e)
+
+
+    @staticmethod
+    def help_show():
+        View.help_show()
+
+    @staticmethod
+    def help_add():
+        View.help_add()
+
+    @staticmethod
+    def help_save():
+        View.help_save()
+
+    @staticmethod
+    def help_select():
+        View.help_select()
+
+    @staticmethod
+    def help_quit():
+        View.help_quit()
+
+    def do_quit(self, line):
+        arg = str(line).lower()
+        if not arg == "-f" and not len(self._std.new_data) == 0:
+            View.warning("The new data hasn't been saved. En ter \"quit -f\" to quit without saving.")
+        else:
+            View.display("Thanks for using Console. Bye!")
+            return True
+
+        
+if __name__ == "__main__":
+    ctl = CLI()
+    ctl.cmdloop()
 
