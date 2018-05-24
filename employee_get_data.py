8b4741e548c4daad32febf2930524c4c049c1a35
Vaishali <vaishali81patel@gmail.com>
2018-05-24 22:23:39 +1200

    CodeSmell "Refuse Bequest" solved!

--- employee_get_data.py
+++ employee_get_data.py
@@ -5,6 +5,10 @@ from employee_data import EmployeeData
 
 class GetEmployee(EmployeeData):
 
+    def __init__(self):
+        print ('Delegation works!!.')
+        super ().__init__()
+
     def get_all_data(self):
         return self.data + self.new_data
 
@@ -43,6 +47,20 @@ class GetEmployee(EmployeeData):
             else:
                 bmi[row[Data.BMI.name]] += 1
         return bmi
+
+    def get_salary(self):
+        salary = {}
+
+        all_data = self.get_all_data()
+        if len(all_data) == 0:
+            return salary
+
+        for row in all_data:
+            if row[Data.SALARY.name] not in salary.keys():
+                salary[row[Data.SALARY.name]] = 1
+            else:
+                salary[row[Data.SALARY.name]] += 1
+        return salary
 
     def __del__(self):
         self.data = []
